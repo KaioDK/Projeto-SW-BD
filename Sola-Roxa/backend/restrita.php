3999d67df95dddcc6ff412865238de756a5b7464
KaioDK <klmeronho@gmail.com>
2025-11-14 17:35:01 -0300

    commit

--- Sola-Roxa/backend/restrita.php
+++ Sola-Roxa/backend/restrita.php
@@ -21,7 +21,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PAINEL DE CONTROLE</title>
+    <title>area restrita</title>
 </head>
 <body>
     <h1>VOCÊ ESTÁ EM UMA ÁREA RESTRITA!</h1>
